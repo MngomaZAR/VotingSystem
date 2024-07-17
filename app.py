59578829f95b96bf0042b9b29086a968158dd8d6
MngomaZAR <122445239+MngomaZAR@users.noreply.github.com>
2024-07-17 07:20:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def ussd():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Run on all interfaces
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
